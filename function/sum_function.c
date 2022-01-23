25e9f2dea570c368bab7197048a47e7bcfb1e1c1
Anurag singh chauhan <anuragchauhan766@gmail.com>
2022-01-23 13:54:09 +0530

    commit 3

--- function/sum_function.c
+++ function/sum_function.c
@@ -1,22 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 
-    int sum(int a, int b); //funtion declaration
+int sum(int a, int b); //funtion declaration
 
-int main(){
-    int c,a,b;
-    
+int main()
+{
+    int c, num1, num2;
+
     printf("Enter the value of a and b \n");
-    scanf("%d%d",&a,&b);
-    
-    c = sum(a,b);
-    printf("%d",c);
-    
+    scanf("%d%d", &num1, &num2);
+
+    c = sum(num1, num2);
+    printf("%d", c);
+
     return 0;
 }
 
-int sum(int a, int b){
+int sum(int a, int b)
+{
     int result;
-    
-    result = a+b;
-    return result;
+
+    // result = a + b;
+    // return result;
+    return a + b;
 }
